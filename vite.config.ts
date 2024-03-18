29b45a69cad46af3550a6e9d1d674dc508a161f9
Loker <aka.loker@gmail.com>
2024-03-18 20:07:58 +0300

    Обновил конфиги

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Unfonts from 'unplugin-fonts/vite';
 
 export default defineConfig(({ mode }) => {
   return {
     base: mode === 'production' ? '/trello-demo/' : '/',
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      Unfonts({
+        google: {
+          families: ['Montserrat'],
+        },
+      }),
+    ],
 
     resolve: {
       alias: {
+        '@': resolve(__dirname, 'src'),
         '@views': resolve(__dirname, 'src', 'views'),
         '@store': resolve(__dirname, 'src', 'store'),
         '@routes': resolve(__dirname, 'src', 'routes'),
