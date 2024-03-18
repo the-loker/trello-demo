dc15eccc71899c3302ee62ab41b6250389b1e511
Loker <aka.loker@gmail.com>
2024-03-18 20:07:00 +0300

    Обновил компонент board-stage

--- src/components/board-stage/board-stage.vue
+++ src/components/board-stage/board-stage.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
-  import { toRefs, defineProps } from 'vue';
+  import { ref, toRefs, defineProps } from 'vue';
+
   import { stageProps } from './board-stage';
   import { useStage } from './use-board-stage';
+
+  import BoardCardForm from '../board-card-form/board-card-form.vue';
 
   const props = defineProps(stageProps);
 
   useStage(props);
 
   const { stage } = toRefs(props);
+  const isOpenForm = ref(false);
+
+  function onOpenForm() {
+    isOpenForm.value = true;
+  }
+
+  function onCloseForm() {
+    isOpenForm.value = false;
+  }
 </script>
 
 <template>
@@ -16,6 +28,17 @@
       <span class="board-stage__title">{{ stage.title }}</span>
     </div>
     <div class="board-stage__cards"></div>
+    <div class="board-stage__form">
+      <template v-if="isOpenForm">
+        <BoardCardForm
+          v-model="isOpenForm"
+          :stage-id="stage.id"
+          @close-form="onCloseForm"
+        />
+      </template>
+
+      <template v-else><button @click="onOpenForm">add</button></template>
+    </div>
   </div>
 </template>
 
@@ -25,10 +48,16 @@
     }
 
     &__title {
+      text-transform: uppercase;
     }
 
     &__cards {
+      background-color: #292b31;
+    }
+
+    &__form {
     }
   }
+
 </style>
 ./use-board-stage./board-stage
