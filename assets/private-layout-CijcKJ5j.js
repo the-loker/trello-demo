16d2e5e6815503fc55f66ea4acf486f8a3c76089
Loker <aka.loker@gmail.com>
2024-03-20 21:19:15 +0300

    Updates

--- assets/private-layout-CijcKJ5j.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./header.vue_vue_type_style_index_0_lang-CgNCG2v0.js";import{d as t,o,c as s,a as r,b as n,r as c,F as _}from"./index-DhxZ2PwS.js";const l={class:"container"},f=t({__name:"private-layout",setup(m){return(e,d)=>(o(),s(_,null,[r(a),n("div",l,[c(e.$slots,"default")])],64))}});export{f as default};
