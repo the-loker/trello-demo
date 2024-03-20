4f490ded09709a9729038e1244e9c4e7acda47dc
Loker <aka.loker@gmail.com>
2024-03-20 15:49:57 +0300

    Добавил базовый адаптив для страници Board

--- src/views/private/Board.vue
+++ src/views/private/Board.vue
@@ -12,9 +12,20 @@
   .board {
     display: grid;
 
-    padding: 30px;
-
+    gap: 20px;
     grid-auto-flow: column;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr;
+
+    @media (max-width: 992px) {
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+    }
+
+    @media (max-width: 576px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: repeat(4, 1fr);
+    }
   }
 
 </style>
